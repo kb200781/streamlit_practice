4d863b11b50d1c8d7bf99169efb5bea56102e28f
Kushagar Bansal <101501748+kb200781@users.noreply.github.com>
2022-11-22 13:48:32 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,6 +5,6 @@ st.title("Twitter Sentiment Analysis")
 st.subheader("Hey there!!! :wave: Welcome to our service")
 
 st.write("\n")
-st.subheader("Wondering what we do? \n")
+st.subheader("Wondering what we do?")
 st.markdown("We **_analyze_** the tweets for you guys to")
 st.write("1. Clear your doubts\n 2. Authenticity of rumors\n 3. Give you a topic for your gossips\n 4. Help in your philosophical research\n and much more!!!")
