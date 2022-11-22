67118b8338cbb4266c513df85525a3af86ba2c18
Kushagar Bansal <101501748+kb200781@users.noreply.github.com>
2022-11-22 14:33:17 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import webbrowser
 
 st.set_page_config(page_title="Twitter Sentiment Analysis", page_icon=":tada:")
 
@@ -14,3 +15,21 @@ if selected == "Home":
     st.subheader("Wondering what we do?")
     st.markdown("We **_analyze_** the tweets for you guys to")
     st.write("1. Clear your doubts\n 2. Authenticity of rumors\n 3. Give you a topic for your gossips\n 4. Help in your philosophical research\n and much more!!!")
+
+if selected == "How to use":
+    st.title("You are in how to use")
+
+if selected == "Analyze":
+    st.title("You are in analyze")
+    
+    genre = st.radio("What do you want to do",
+                     ("I want to analyze the tweets of a twitter account", "I want to analyze a topic", "I want to analyze my dataset"))
+    
+    if genre == "I want to analyze the tweets of a twitter account":
+        st.write("You selected first")
+    
+    if genre == "I want to analyze a topic":
+        st.write("you selected 2")
+        
+    if genre == "I want to analyze my dataset":
+        st.write("you selected 3")
