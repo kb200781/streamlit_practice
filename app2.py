fe0eea21cb2bb873bd4fecb32d35a37d2adb7d65
Kushagar Bansal <101501748+kb200781@users.noreply.github.com>
2022-11-22 13:45:21 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,5 +8,5 @@ st.subheader("Welcome to our service")
 st.write("\n \n")
 
 st.subheader("Wondering what we do? \n")
-st.marksown("We **_analyze_** the tweets for you guys to")
+st.markdown("We **_analyze_** the tweets for you guys to")
 st.write("1. Clear your doubts\n 2. Authenticity of rumors\n 3. Give you a topic for your gossips\n 4. Help in your philosophical research\n and much more!!!")
