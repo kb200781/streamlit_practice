c0b44ce5ef24392161adee544b8046d78821096a
Kushagar Bansal <101501748+kb200781@users.noreply.github.com>
2022-11-22 13:50:22 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,3 +7,8 @@ st.subheader("Hey there!!! :wave: Welcome to our service")
 st.subheader("Wondering what we do?")
 st.markdown("We **_analyze_** the tweets for you guys to")
 st.write("1. Clear your doubts\n 2. Authenticity of rumors\n 3. Give you a topic for your gossips\n 4. Help in your philosophical research\n and much more!!!")
+
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
