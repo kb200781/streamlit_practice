dc46c2712d904478e3e07fe031e675120d1b67ef
Kushagar Bansal <101501748+kb200781@users.noreply.github.com>
2022-11-22 13:50:49 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title="Twitter Sentiment Analysis", page_icon=":tada:")
 st.title("Twitter Sentiment Analysis")
