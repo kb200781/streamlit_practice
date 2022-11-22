fbccb62a86d757967c448708d4ccc433b5eb9e10
Kushagar Bansal <101501748+kb200781@users.noreply.github.com>
2022-11-22 13:53:01 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,15 +1,16 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+st.set_page_config(page_title="Twitter Sentiment Analysis", page_icon=":tada:")
+
+selected = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
 
-st.set_page_config(page_title="Twitter Sentiment Analysis", page_icon=":tada:")
-st.title("Twitter Sentiment Analysis")
-st.subheader("Hey there!!! :wave: Welcome to our service")
+if selected == "Home":
+    st.title("Twitter Sentiment Analysis")
+    st.subheader("Hey there!!! :wave: Welcome to our service")
 
-st.subheader("Wondering what we do?")
-st.markdown("We **_analyze_** the tweets for you guys to")
-st.write("1. Clear your doubts\n 2. Authenticity of rumors\n 3. Give you a topic for your gossips\n 4. Help in your philosophical research\n and much more!!!")
-
+    st.subheader("Wondering what we do?")
+    st.markdown("We **_analyze_** the tweets for you guys to")
+    st.write("1. Clear your doubts\n 2. Authenticity of rumors\n 3. Give you a topic for your gossips\n 4. Help in your philosophical research\n and much more!!!")
