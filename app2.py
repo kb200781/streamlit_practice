c58e75a2440b962b1295a006361f48ad18ddc9d6
Kushagar Bansal <101501748+kb200781@users.noreply.github.com>
2022-11-22 13:41:05 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,20 +1,12 @@
-# %%writefile app.py
-import streamlit as st
-from streamlit_option_menu import option_menu
+import streamlit
 
-# horizontal menu
-selected = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
+st.set_page_config(page_title="Twitter Sentiment Analysis", page_icon=":tada:")
+st.title("Twitter Sentiment Analysis")
+st.subheader("Hey there!!! :wave:")
+st.subheader("Welcome to our service")
 
-if selected == "Home":
-  st.write("You are at home")
+st.write("\n \n")
 
-if selected == "Upload":
-  st.write("You are at Upload")
-
-if selected == "Tasks":
-  st.write("You are at tasks")
-
-if selected == "Settings":
-  st.write("You are at settings")
+st.subheader("Wondering what we do? \n")
+st.marksown("We **_analyze_** the tweets for you guys to")
+st.write("1. Clear your doubts\n 2. Authenticity of rumors\n 3. Give you a topic for your gossips\n 4. Help in your philosophical research\n and much more!!!")
